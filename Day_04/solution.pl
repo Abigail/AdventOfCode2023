88756cb14b2dfb03f7b7c151e995cd20821cf384
Abigail <abigail@abigail.freedom.nl>
2023-12-04 20:31:21 +0100

    Advent of code 2023, day 4: Rename variable.

--- Day_04/solution.pl
+++ Day_04/solution.pl
@@ -17,11 +17,11 @@ while (<>) {
     my %drawn = map {$_ => 1}      $drawn =~ /[0-9]+/g;
     my $match = grep {$drawn {$_}} $card  =~ /[0-9]+/g;
 
-    $solution_2 += my $score = 1 + (shift (@cards) // 0);
+    $solution_2 += my $instances = 1 + (shift (@cards) // 0);
 
     if ($match) {
         $solution_1 += 2 ** ($match - 1);
-        $cards [$_] += $score for 0 .. $match - 1;
+        $cards [$_] += $instances for 0 .. $match - 1;
     }
 }
 

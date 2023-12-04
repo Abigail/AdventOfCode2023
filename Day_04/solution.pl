e68ace5653b0b0384dc9605ab461079de8e69c00
Abigail <abigail@abigail.freedom.nl>
2023-12-04 20:25:13 +0100

    Advent of Code 2023, day 4: Simplify.

--- Day_04/solution.pl
+++ Day_04/solution.pl
@@ -15,14 +15,13 @@ my @cards;
 while (<>) {
     (undef, my ($drawn, $card)) = split /[:|]/;
     my %drawn = map {$_ => 1}      $drawn =~ /[0-9]+/g;
-    my @match = grep {$drawn {$_}} $card  =~ /[0-9]+/g;
+    my $match = grep {$drawn {$_}} $card  =~ /[0-9]+/g;
 
-    $cards [0] ++;
-    $solution_2 += my $score = shift @cards;
+    $solution_2 += my $score = 1 + (shift (@cards) // 0);
 
-    if (@match) {
-        $solution_1 += 2 ** (@match - 1);
-        $cards [$_] += $score for keys @match;
+    if ($match) {
+        $solution_1 += 2 ** ($match - 1);
+        $cards [$_] += $score for 0 .. $match - 1;
     }
 }
 

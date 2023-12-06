d8041f058bd3e938f1ae58fff46d09bd5246ca68
Abigail <abigail@abigail.freedom.nl>
2023-12-06 11:31:22 +0100

    Advent of Code: Template.
    Added a template for advent of code solutions.

---
+++ solution.pl
@@ -0,0 +1,21 @@
+#!/opt/perl/bin/perl
+
+use 5.038;
+
+use strict;
+use warnings;
+no  warnings 'syntax';
+
+use experimental 'for_list';
+
+@ARGV = "input" unless @ARGV;
+
+my $solution_1 = 0;
+my $solution_2 = 0;
+
+
+say "Solution 1: $solution_1";
+say "Solution 2: $solution_2";
+
+
+__END__
